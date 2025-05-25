87abb422072d4e66808678e57186dee2a6bba6ba
Nixon Migodhi <migodhi@gmail.com>
2025-05-25 18:12:22 +0200

    Add a loop to language

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,2 +1,14 @@
-const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
-console.log(lines.join('\n'));
+// 6-multi_languages_loop.js
+
+// Define an array of strings containing the messages
+const messages = [
+  "C is fun",
+  "Python is cool",
+  "JavaScript is amazing"
+];
+
+// Loop through the array and print each message
+for (let i = 0; i < messages.length; i++) {
+  console.log(messages[i]);
+}
+
